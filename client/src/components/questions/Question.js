437af9e644d609c10645d0933ef2ad7a8d255db8
Darius Raazi <dariusraazi@icloud.com>
2019-08-01 15:19:23 -0600

    slight styling change

--- client/src/components/questions/Question.js
+++ client/src/components/questions/Question.js
@@ -7,7 +7,7 @@ const Question = props => {
     console.log(props)
     return(
         <Link to={`/questions/${props._id}`} className={Styles.questionLinks}>
-            <button className={Styles.questionVote} onClick={() => props.upVoter(props._id)}>Vote</button>
+            {/* <button className={Styles.questionVote} onClick={() => props.upVoter(props._id)}>Vote</button> */}
             <p>{props.question}</p>
             <span>{props.vote}</span>
         </Link>
